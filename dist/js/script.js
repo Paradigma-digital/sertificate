5ef30435734713ff9609b0668c9a2a6816f28ee6
Paradigma-digital <85119803+Paradigma-digital@users.noreply.github.com>
2021-11-25 14:02:26 +0300

    add table for place

--- dist/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(document.querySelector('.place_value').innerHTML.length)
-    document.querySelector('.place_info').style.left = `${document.querySelector('.place_value').innerHTML.length * 127}px`;
-})
